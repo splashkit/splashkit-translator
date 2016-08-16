cf5d83851e58116f6f5d94788f37df886e670a9d
Alex Cummaudo <alexcu@me.com>
2016-08-16 14:12:32 +1000

    QUALITY: Refactor types template for __skadapter prefix
    Also introduce make_[foo]_adapter

--- res/generators/sklibc/types.tpl.cpp
+++ res/generators/sklibc/types.tpl.cpp
@@ -1,15 +1,20 @@
-#define ptr void *
-#define __to_ptr(value)\
-(ptr)value
-#define __to_bool(value)\
+#define __skadapter__to_sklib_bool(value)\
+value == true ? 1 : 0
+#define __skadapter__to_bool(value)\
 value == 1 ? true : false
-#define __no_type_change(type)\
-type __to_##type(type value) { return value; }
-#define __sk_type_casting(type)\
-type __to_##type(ptr value) { return static_cast<type>(value); }
-#define __array_wrappable(type)\
-typedef struct { type *data; int size; } __sklib_##type##_array;
 
-__no_type_change(int)
-__no_type_change(float)
-__no_type_change(double)
+#define __skadapter__make_direct_adapter(type)\
+type __skadapter__to_sklib_##type(type value) { return value; }\
+type __skadapter__to_##type(type value) { return value; }
+__skadapter__make_direct_adapter(int)
+__skadapter__make_direct_adapter(float)
+__skadapter__make_direct_adapter(double)
+
+#define __sklib_ptr void *
+#define __skadapter__make_typealias_adapter(type)\
+__sklib_ptr __skadapter__to_sklib_##type(type value) { return static_cast<__sklib_ptr>(value); }\
+type __skadapter__to_##type(__sklib_ptr value) { return static_cast<type>(value); }
+
+#define __skadapter__make_enum_adapter(type)\
+int __skadapter__to_sklib_##type(type value) { return static_cast<int>(value); }\
+type __skadapter__to_##type(int value) { return static_cast<type>(value); }
