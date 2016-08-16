0e1aabd105739b9d69289024ed65e86ee9cee100
Alex Cummaudo <alexcu@me.com>
2016-08-16 14:12:16 +1000

    QUALITY: Refactor strings template with __skadapter prefix

--- res/generators/sklibc/strings.tpl.cpp
+++ res/generators/sklibc/strings.tpl.cpp
@@ -1,17 +1,17 @@
 typedef struct { char *string; int size; } __sklib_string;
-__sklib_string __to_sklib_string(std::string s)
+__sklib_string __skadapter__to_sklib_string(std::string s)
 {
-  __sklib_string result;
-  result.size = s.length();
-  result.string = (char *)malloc(result.size + 1);
-  strcpy(result.string, s.c_str());
-  return result;
+    __sklib_string result;
+    result.size = s.length();
+    result.string = (char *)malloc(result.size + 1);
+    strcpy(result.string, s.c_str());
+    return result;
 }
-void __sklib_free_sklib_string(__sklib_string s)
+void __skadapter__free_sklib_string(__sklib_string s)
 {
-  free(s.string);
+    free(s.string);
 }
-std::string __to_string(__sklib_string s)
+std::string __skadapter__to_string(__sklib_string s)
 {
-  return std::string(s.string);
+    return std::string(s.string);
 }
