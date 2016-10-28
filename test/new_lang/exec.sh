845ac476ed7c4089b1624a1bcc889dd7e4b9a010
Alex Cummaudo <alexcu@me.com>
2016-10-28 15:31:30 +1100

    CONFIG: Ensure rpath is correct for test

--- test/new_lang/exec.sh
+++ test/new_lang/exec.sh
@@ -5,7 +5,7 @@ echo "Generate adapter"
 ../../translate --generate clib,pascal -i "test${LANG_TEST}/new_lang.h" --output ../out -l
 
 echo "Make dynamic library"
-clang++ -shared -g -DBUILDING_SK_LIB -std=c++14 -I "test${LANG_TEST}" "test${LANG_TEST}/new_lang.cpp" ../out/clib/sk_clib.cpp ../out/clib/lib_type_mapper.cpp -I../clib -I../.. -I. -o libSplashKit.dylib
+clang++ -shared -g -DBUILDING_SK_LIB -std=c++14 -I "test${LANG_TEST}" "test${LANG_TEST}/new_lang.cpp" ../out/clib/sk_clib.cpp ../out/clib/lib_type_mapper.cpp -I../clib -I../.. -I. -o libSplashKit.dylib -install_name @rpath/libSplashKit.dylib
 
 echo "Compile program"
-ppcx64 -g -FE. -Fu../out/pascal -S2 test${LANG_TEST}/TestProgram.pas -k-L. -k-lSplashKit
+ppcx64 -g -FE. -Fu../out/pascal -S2 test${LANG_TEST}/TestProgram.pas -k-L. -k-lSplashKit -k"-rpath @loader_path"
