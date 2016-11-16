d55aae92113a15c51bd3c8952d77e1dff750f4d1
Andrew Cain <acain@swin.edu.au>
2016-11-17 08:51:02 +1100

    FIX: Correct git ignore for python test programs

---
+++ test/new_lang/test5/test_program.py
@@ -0,0 +1,7 @@
+from splashkit import *
+
+arr = get_bools()
+print_all(arr)
+print('Adding false')
+add_bool(arr, False)
+print_all(arr)
---
+++ test/new_lang/test6/test_program.py
@@ -0,0 +1,5 @@
+from splashkit import *
+
+v = get_vector()
+print_vector(v)
+print("Matches: ", v.x, ',', v.y, ' and ', v.multi_name)
