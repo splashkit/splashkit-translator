00e03d28b9354663e4126a95cc80d96fca82432f
Andrew Cain <macite@gmail.com>
2021-06-15 21:50:06 +1000

    fix: Compile to 64bit only and add rust language

--- test/new_lang/exec.sh
+++ test/new_lang/exec.sh
@@ -2,14 +2,17 @@
 
 LANG_TEST=$1
 echo "Generate adapter"
-../../translate --generate clib,pascal,cpp,python,csharp -i "test${LANG_TEST}/new_lang.h" --output ../out -l
+../../translate --generate clib,pascal,cpp,python,csharp,rust -i "test${LANG_TEST}/new_lang.h" --output ../out -l
 
 echo "Make dynamic library"
-clang++ -arch i386 -shared -g -DBUILDING_SK_LIB -std=c++14 -I "test${LANG_TEST}" "test${LANG_TEST}/new_lang.cpp" ../out/clib/sk_clib.cpp ../out/clib/lib_type_mapper.cpp -I../clib -I../.. -I. -o libSplashKit-i386.dylib -install_name @rpath/libSplashKit.dylib
+# clang++ -arch i386 -shared -g -DBUILDING_SK_LIB -std=c++14 -I "test${LANG_TEST}" "test${LANG_TEST}/new_lang.cpp" ../out/clib/sk_clib.cpp ../out/clib/lib_type_mapper.cpp -I../clib -I../.. -I. -o libSplashKit-i386.dylib -install_name @rpath/libSplashKit.dylib
 
-clang++ -arch x86_64 -shared -g -DBUILDING_SK_LIB -std=c++14 -I "test${LANG_TEST}" "test${LANG_TEST}/new_lang.cpp" ../out/clib/sk_clib.cpp ../out/clib/lib_type_mapper.cpp -I../clib -I../.. -I. -o libSplashKit-x64.dylib -install_name @rpath/libSplashKit.dylib
+# clang++ -arch x86_64 -shared -g -DBUILDING_SK_LIB -std=c++14 -I "test${LANG_TEST}" "test${LANG_TEST}/new_lang.cpp" ../out/clib/sk_clib.cpp ../out/clib/lib_type_mapper.cpp -I../clib -I../.. -I. -o libSplashKit-x64.dylib -install_name @rpath/libSplashKit.dylib
 
-lipo -create libSplashKit-i386.dylib libSplashKit-x64.dylib -output libSplashKit.dylib
+# lipo -create libSplashKit-i386.dylib libSplashKit-x64.dylib -output libSplashKit.dylib
+
+clang++ -arch x86_64 -shared -g -DBUILDING_SK_LIB -std=c++14 -I "test${LANG_TEST}" "test${LANG_TEST}/new_lang.cpp" ../out/clib/sk_clib.cpp ../out/clib/lib_type_mapper.cpp -I../clib -I../.. -I. -o libSplashKit.dylib -install_name @rpath/libSplashKit.dylib
+
 
 function run_cpp
 {
@@ -68,6 +71,22 @@ echo "Do you wish to run the Python version?"
 select yn in "Yes" "No"; do
     case $yn in
         Yes ) run_python; break;;
+        No ) break;;
+    esac
+done
+
+function run_rust
+{
+  cp "test${LANG_TEST}/test_program.rs" .
+  echo "Running Rust program"
+  rustc -C link-args="-L . -Wl,-rpath,@loader_path" ./test${LANG_TEST}/test_program.rs
+  ./test_program
+}
+
+echo "Do you wish to run the Rust version?"
+select yn in "Yes" "No"; do
+    case $yn in
+        Yes ) run_rust; break;;
         No ) break;;
     esac
 done
